894eb730518151885fb4dae9f56694afb60a62b7
lein <xmllein@163.com>
2023-10-12 14:55:27 +0800

    refactor(custom): 生成使用mock数据

--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,10 @@ const rootWidth = 390
 const deviceWidth = document.documentElement.clientWidth
 document.documentElement.style.fontSize = (rootValue * deviceWidth) / rootWidth + 'px'
 app.mount('#app')
+
+// production mock server (生产环境打包 mock 数据, 用于展示，正式生产务必删除！！！)
+if (process.env.NODE_ENV === 'production') {
+  import('./mockProdServer').then(({ setupProdMockServer }) => {
+    setupProdMockServer()
+  })
+}
